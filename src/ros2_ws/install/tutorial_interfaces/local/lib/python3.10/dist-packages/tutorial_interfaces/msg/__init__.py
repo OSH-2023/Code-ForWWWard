909346ffb6fbf785c87d9724e3b7ceb14e38bec5
CaptchaAC <smgj@mail.ustc.edu.cn>
2023-07-07 09:37:55 +0800

    delete files that 'filename too long'

--- src/ros2_ws/install/tutorial_interfaces/local/lib/python3.10/dist-packages/tutorial_interfaces/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from tutorial_interfaces.msg._num import Num  # noqa: F401
-from tutorial_interfaces.msg._sphere import Sphere  # noqa: F401
--- src/ros2_ws/install/tutorial_interfaces/local/lib/python3.10/dist-packages/tutorial_interfaces/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from tutorial_interfaces.srv._add_three_ints import AddThreeInts  # noqa: F401
